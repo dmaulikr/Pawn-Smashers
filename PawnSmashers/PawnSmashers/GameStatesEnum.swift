ae45d1abc45c855f712b986417f7b13e6c0f12be
student <tarazevits@gmail.com>
2016-11-15 17:51:42 -0500

    Added physics bodies to all scene objects, created Node classes for scoring circle

---
+++ PawnSmashers/PawnSmashers/GameStatesEnum.swift
@@ -0,0 +1,19 @@
+//
+//  GameStatesEnum.swift
+//  PawnSmashers
+//
+//  Created by student on 11/15/16.
+//  Copyright © 2016 student. All rights reserved.
+//
+
+import Foundation
+
+enum GameStates {
+    case Setup1
+    case Setup2
+    case Shooting1
+    case Shooting2
+    case Menu
+    case RoundEnd
+    case GameEnd
+}
---
+++ PawnSmashers/PawnSmashers/ScoreRing.swift
@@ -0,0 +1,23 @@
+//
+//  ScoreRing.swift
+//  PawnSmashers
+//
+//  Created by student on 11/15/16.
+//  Copyright © 2016 student. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+class ScoreRing : SKSpriteNode {
+    
+    let scoreValue:Int
+    
+    
+    
+    func didMoveToScene() {
+        
+    }
+    
+    
+    
+}
